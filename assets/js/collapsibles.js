db13da3a0ca0e75ed537c0682f71f1d79a9ce334
SummerTold <fabien.petit@univ-amu.fr>
2021-06-07 17:04:00 +0200

    JS open first paper

--- assets/js/collapsibles.js
+++ assets/js/collapsibles.js
@@ -1,6 +1,9 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+coll[0].classList.toggle("active");
+coll[0].nextElementSibling.style.maxHeight = coll[0].nextElementSibling.scrollHeight + "px";
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
